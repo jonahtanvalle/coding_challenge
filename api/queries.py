1d6fb6304626256629777b05434b708861202c5b
Jonahtan Valle <jonahtanvalle@gmail.com>
2024-04-03 13:14:43 -0500

    feat: stable queries exposing version.

---
+++ api/queries.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+# Database connection details (replace with yours)
+DATABASE_URL =  'postgresql://postgres:postgres@postgres/postgres'
+
+# Create engine and session maker
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_department_hiring_data():
+    session = SessionLocal()
+
+    query = text("""
+        WITH base AS (
+            SELECT
+                department_id,
+                COUNT(id) hired_q
+            FROM
+                public.employees
+            WHERE
+                EXTRACT(YEAR FROM TO_DATE(datetime, 'YYYY-MM-DD"T"HH24:MI:SS"Z"')) = 2021
+            GROUP BY
+                department_id
+        ),
+        averages AS (
+            SELECT
+                AVG(hired_q) average
+            FROM
+                base
+        )
+        SELECT
+            base.department_id,
+            d.department,
+            base.hired_q AS hired
+        FROM
+            base
+        LEFT JOIN public.departments d ON base.department_id = d.id
+        JOIN averages ON averages.average < base.hired_q
+        ORDER BY
+            hired_q DESC;
+    """)
+
+    rows = session.execute(query)
+
+    session.close()
+
+    return [{'department_id': row[0], 'department': row[1], 'hired': row[2]} for row in rows]
